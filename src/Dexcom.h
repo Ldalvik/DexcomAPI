57faf54743db3b8fb35171aa8d5c4da7382d84cf
Ldalvik <subaruforge@gmail.com>
2024-03-20 08:57:57 -0400

    Cleanup code

--- src/Dexcom.h
+++ src/Dexcom.h
@@ -17,37 +17,40 @@ class Dexcom
 {
 public:
     Dexcom(const char *username, const char *password, bool ous = false);
+    Dexcom(bool ous = false);
     bool createSession();
     bool update();
     // bool updateList(int minutes, int maxCount);
     void setLowThreshold(int threshold);
     void setHighThreshold(int threshold);
-    void setWarningThreshold(int threshold);
+    void setUrgentLowThreshold(int low);
+    void setUrgentHighThreshold(int low);
       
     int currentGlucose = 0;
-    TrendTypes currentTrend;
-    AdvTrendTypes currentAdvTrend;
-    RangeTypes currentRange;
-    StatusTypes currentStatus = LOGGED_OUT;
+    GlucoseTrend_t     currentTrend;
+    AdvGlucoseTrend_t  currentAdvTrend;
+    GlucoseRange_t     currentRange;
+    DexcomStatus_t     currentStatus = LOGGED_OUT;
 
-    int lowThreshold = 90;     // default value for below range
-    int highThreshold = 250;   // default value for above range
-    int warningThreshold = 20; // threshold above or below range for warning 
+    int highThreshold       = 250; // default value for above range
+    int urgentHighThreshold = 300; // default high threshold urgent alarm
+    int lowThreshold        = 90;  // default value for below range
+    int urgentLowThreshold  = 80;  // default low threshold for urgent alarm
+
     const char *_username;
+    const char *_password;
 
 private:
-    String post(const char *url, const char *postData);
-    String getAccountId();
-    String getSessionId(String accountId);
-    String stripToken(String data);
-    TrendTypes getTrendType(String trend);
-    AdvTrendTypes getAdvTrendType();
-    RangeTypes getRange();
+    String            post(const char *url, const char *postData);
+    String            getAccountId();
+    String            getSessionId(String accountId);
+    String            stripToken(String data);
+    GlucoseTrend_t    getTrendType(String trend);
+    AdvGlucoseTrend_t getAdvTrendType();
+    GlucoseRange_t    getRange();
 
     String _session_id;
     String _base_url = DEXCOM_BASE_URL;
-
-    const char *_password;
 };
 
 #endif
